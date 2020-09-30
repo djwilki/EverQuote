a95c3d25a07cc6fd934a0d24b6b54e34cd46607f
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-30 17:38:28 -0400

    Completed Heroku deployment

---
+++ EverQuote/backend/app/static/precache-manifest.436bf26c96184fb234ca3049dd7b11b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f33d2e22a15dad059e6b5a1348e36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2194a3bee68ddb9914f4",
+    "url": "/static/css/main.6160e248.chunk.css"
+  },
+  {
+    "revision": "4773d956f3e65c65355a",
+    "url": "/static/js/2.a4bfa5cc.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.a4bfa5cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2194a3bee68ddb9914f4",
+    "url": "/static/js/main.ed4dc509.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

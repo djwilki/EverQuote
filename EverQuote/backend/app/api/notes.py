3db18da9fce9cdc8072015467870113af08bb190
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-10-01 16:12:26 -0400

    Updated TextEditor logic for autosave

--- EverQuote/backend/app/api/notes.py
+++ EverQuote/backend/app/api/notes.py
@@ -10,3 +10,12 @@ def new_note():
     db.session.add(note)
     db.session.commit()
     return note.to_dict()
+
+@notes.route("/<int:note_id>", methods=["PUT"])
+def update_note(note_id):
+    data = request.json
+    note = Note.query.filter(Note.id == note_id).first()
+    note["content"] = data['content']
+    note["title"] = data["title"]
+    db.session.commit()
+    return note.to_dict()
--- EverQuote/client/src/components/TextEditor.js
+++ EverQuote/client/src/components/TextEditor.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { updateNote } from '../store/notes';
 
 
 const TextEditor = ({ activeNoteObj }) => {
+    const dispatch = useDispatch();
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    let autosaveTimeout = useRef(null);
 
     useEffect(() => {
         setTitle(activeNoteObj.title);
@@ -19,12 +22,22 @@ const TextEditor = ({ activeNoteObj }) => {
         setContent(event.target.value);
     }
 
+    const handleAutoSave = event => {
+        if (autosaveTimeout) {
+            clearTimeout(autosaveTimeout);
+        }
+        autosaveTimeout = setTimeout(async () => {
+            const res = await dispatch(updateNote(activeNoteObj.id, title, content));
+        }, 750);
+        return;
+    }
+
     return (
         <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh" }}>
             <div style={{ minHeight: "92px", backgroundColor: "white", border: "1px solid #F2F2F2" }}>
                 Toolbar Placeholder
             </div>
-            <form style={{ display: "flex", flexDirection: "column", width: "100%", height: "799px", border: "1px solid #F2F2F2" }}>
+            <form style={{ display: "flex", flexDirection: "column", width: "100%", height: "799px", border: "1px solid #F2F2F2" }} onKeyPress={handleAutoSave}>
                 <input type="text" style={{ height: "8%", border: "none" }} value={title} onChange={handleTitleChange} />
                 <textarea rows="8" style={{ height: "92%", border: "none" }} value={content} onChange={handleContentChange}></textarea>
             </form>

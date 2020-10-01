7f2f5c2ef935d12204b1da78d11eb7b9c106ea66
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-10-01 15:57:56 -0400

    Text Editor populates with active note content

--- EverQuote/client/src/components/TextEditor.js
+++ EverQuote/client/src/components/TextEditor.js
@@ -1,23 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 
 const TextEditor = ({ activeNoteObj }) => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        setTitle(activeNoteObj.title);
+        setContent(activeNoteObj.content);
+    }, [activeNoteObj]);
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleContentChange = event => {
+        setContent(event.target.value);
+    }
 
     return (
-        <div style={{display: "flex", flexDirection: "column", width: "100%", height: "100vh"}}>
-            <div style={{minHeight: "92px", backgroundColor: "white", border: "1px solid #F2F2F2"}}>
+        <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh" }}>
+            <div style={{ minHeight: "92px", backgroundColor: "white", border: "1px solid #F2F2F2" }}>
                 Toolbar Placeholder
             </div>
-        <form style={{display: "flex", flexDirection: "column", width: "100%", height: "799px", border: "1px solid #F2F2F2"}}>
-            <input type="text" style={{height: "8%", border: "none"}}/>
-            <textarea rows="8" style={{height: "92%", border: "none"}}></textarea>
-        </form>
-        <div style={{backgroundColor: "white", height: "100%"}}>
-            <span>Note saved</span>
-        </div>
+            <form style={{ display: "flex", flexDirection: "column", width: "100%", height: "799px", border: "1px solid #F2F2F2" }}>
+                <input type="text" style={{ height: "8%", border: "none" }} value={title} onChange={handleTitleChange} />
+                <textarea rows="8" style={{ height: "92%", border: "none" }} value={content} onChange={handleContentChange}></textarea>
+            </form>
+            <div style={{ backgroundColor: "white", height: "100%" }}>
+                <span>Note saved</span>
+            </div>
         </div>
     );
+}
+
+TextEditor.defaultProps = {
+    activeNoteObj: {
+        title: "",
+        content: ""
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
--- EverQuote/client/src/store/notes.js
+++ EverQuote/client/src/store/notes.js
@@ -3,6 +3,7 @@ import { setActiveNote } from './session';
 
 const ADD_NOTE = '/notes/ADD_NOTE';
 const SET_NOTES = '/notes/SET_NOTES';
+const UPDATE_NOTE = "/notes/UPDATE_NOTE";
 
 export const setNote = note => {
     return {
@@ -15,6 +16,13 @@ export const setNotes = notes => {
     return {
         type: SET_NOTES,
         notes
+    }
+}
+
+export const updateNoteItem = note => {
+    return {
+        type: UPDATE_NOTE,
+        note
     }
 }
 
@@ -55,13 +63,38 @@ export const addNote = (userId, notebookId) => {
     }
 }
 
+export const updateNote = (noteId, title, content) => {
+    const csrfToken = Cookies.get("XSRF-TOKEN");
+    return async dispatch => {
+        const res = await fetch(`/api/notes/${noteId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFTOKEN": csrfToken
+            },
+            body: JSON.stringify({ noteId, title, content })
+        });
+
+        res.data = await res.json();
+
+        if (res.ok) {
+            dispatch(updateNoteItem(res.data));
+        }
+        return res;
+    }
+}
+
 export default function noteReducer(state = {}, action) {
+    const newState = Object.assign({}, state);
     switch (action.type) {
         case ADD_NOTE:
             const { id } = action.note;
             return { [id]: action.note, ...state };
         case SET_NOTES:
             return { ...action.notes, ...state };
+        case UPDATE_NOTE:
+            newState[action.note.id] = action.note;
+            return newState;
         default:
             return state;
     }

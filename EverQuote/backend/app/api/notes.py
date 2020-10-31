7457c3ee89b5e5feafc7a65e9eb2456fd105e6cb
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-10-31 10:41:52 -0400

    Grab random quote from API and assign its content to note on note creation

--- EverQuote/backend/app/api/notes.py
+++ EverQuote/backend/app/api/notes.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, request
 from app.models import db, Note, Notebook
 from datetime import datetime
@@ -7,7 +8,9 @@ notes = Blueprint("notes", __name__)
 @notes.route("", methods=["POST"])
 def new_note():
     data = request.json
-    note = Note(content="", userId=data['userId'], notebookId=data['notebookId'])
+    note_content = requests.get('https://api.quotable.io/random').json()
+    print(note_content)
+    note = Note(content=note_content["content"], title=f"{note_content['author']} said...", userId=data['userId'], notebookId=data['notebookId'])
     db.session.add(note)
     db.session.commit()
     return note.to_dict()

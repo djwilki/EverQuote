24235d033f9293964d6d39ff144a0aa49c8fe890
Jerel <65048179+jerels@users.noreply.github.com>
2020-11-03 16:33:48 -0600

    active note updates

--- EverQuote/client/src/components/NoteOptionsModal.js
+++ EverQuote/client/src/components/NoteOptionsModal.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import { toggleMoveModal, toggleNoteModal } from '../store/ui';
+import { setActiveNote } from '../store/session';
 import { trashNote } from '../store/trash';
 
 const NoteOptionModal = () => {
     const dispatch = useDispatch();
     const noteId = useSelector(state => state.session.activeNote);
+    const notes = [useSelector(state => state.entities.notes)];
     const note = useSelector(state => state.entities.notes[noteId]);
+
     const handleMoveModal = async e => {
         await dispatch(toggleMoveModal());
         await dispatch(toggleNoteModal());
@@ -17,6 +20,16 @@ const NoteOptionModal = () => {
     const handleTrash = async e => {
         await dispatch(trashNote(noteId));
         note.isTrash = true;
+        for (let i = 0; i < notes.length; i++) {
+            for (let [key, value] of Object.entries(notes[i])) {
+                  if (!value.isTrash) {
+                    debugger;
+                    console.log(value);
+                    await dispatch(setActiveNote(value.id));
+                    return;
+            }
+            }
+        }
         await dispatch(toggleNoteModal());
         return;
     };

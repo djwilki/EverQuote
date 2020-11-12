4ba627ee6612fac3eda4ae7e4394fa66b6e6dfe2
Jerel <65048179+jerels@users.noreply.github.com>
2020-11-12 08:47:05 -0600

    moved the empty trash button over a bit

--- EverQuote/client/src/components/TrashList.js
+++ EverQuote/client/src/components/TrashList.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { setActiveNote } from '../store/session';
-import {toggleEmptyTrash} from '../store/ui';
+import { toggleEmptyTrash } from '../store/ui';
 import NoteCard from './NoteCard';
 import noteStyles from '../styles/note.module.css';
 
-const TrashList = ({hidden}) => {
+const TrashList = ({ hidden }) => {
     const dispatch = useDispatch();
     const notesObj = useSelector(state => state.entities.notes);
     const notes = Object.values(notesObj);
@@ -28,7 +28,7 @@ const TrashList = ({hidden}) => {
                 <div >
                     <h1 className={noteStyles.noteListHeader}>
                         Trash
-                        <button onClick={emptyConfirm} style={{marginLeft: '100px', padding: '8px 15px', overflow: 'hidden', textOverflow: 'ellipsis', backgroundColor: 'transparent', border: '1px solid #737373', color: '#737373', lineHeight: '1', letterSpacing: '0', fontSize: '14px', borderRadius: '4px'}}>
+                        <button onClick={emptyConfirm} style={{ marginLeft: '140px', padding: '8px 15px', overflow: 'hidden', textOverflow: 'ellipsis', backgroundColor: 'transparent', border: '1px solid #737373', color: '#737373', lineHeight: '1', letterSpacing: '0', fontSize: '14px', borderRadius: '4px' }}>
                             Empty Trash
                         </button>
                     </h1>
@@ -36,11 +36,11 @@ const TrashList = ({hidden}) => {
                 <span className={noteStyles.noteAmount}>{trash.length} Notes</span>
             </div>
             <div className={noteStyles.noteList + " noteList"}>
-            { trash.map((note, i) => {
-                return (
-                    <NoteCard key={`note-${i + 1}`} note={note} />
-                );
-            })}
+                {trash.map((note, i) => {
+                    return (
+                        <NoteCard key={`note-${i + 1}`} note={note} />
+                    );
+                })}
             </div>
         </div>
     );

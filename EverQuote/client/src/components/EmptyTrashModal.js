e871fc8a78cab18c7ec8dafd09f8256ee38d8da4
Jerel <65048179+jerels@users.noreply.github.com>
2020-11-12 08:16:12 -0600

    finished styling on the empty trash modal

--- EverQuote/client/src/components/EmptyTrashModal.js
+++ EverQuote/client/src/components/EmptyTrashModal.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { toggleEmptyTrash } from '../store/ui';
 import { emptyUserTrash } from '../store/trash';
-import {  useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 
 const EmptyTrashModal = () => {
@@ -21,33 +21,33 @@ const EmptyTrashModal = () => {
     };
 
     return (
-        <div style={{position: 'fixed', width: '100%', height: '100%', backgroundColor: 'rgba(133, 133, 133, 0.5)', zIndex: '9', margin: '0', padding: '0', boxSizing: 'border-box'}}>
-            <div style={{opacity: '1', transform: 'scale(1)', transition: 'transform 200ms 100ms ease,opacity 200ms 100ms ease', willChange: 'transform', border: '1px solid #ccc', color: '#333', minWidth: '465px', minHeight: '200px', maxWidth: '990px', background: '#fff', display: 'flex', justifyContent: 'center', flexDirection: 'column', boxShadow: '0 0 0 1px rgba(0,0,0,0.08),0 2px 15px rgba(0,0,0,0.2)', borderRadius: '4px'}}>
+        <div style={{ position: 'fixed', width: '100%', height: '100%', backgroundColor: 'rgba(133, 133, 133, 0.5)', zIndex: '9', margin: '0', padding: '0', boxSizing: 'border-box' }}>
+            <div style={{ border: '1px solid #ccc', color: '#333', minWidth: '465px', minHeight: '200px', maxWidth: '990px', background: '#fff', display: 'flex', justifyContent: 'center', flexDirection: 'column', boxShadow: '0 0 0 1px rgba(0,0,0,0.08),0 2px 15px rgba(0,0,0,0.2)', borderRadius: '4px', width: '800px', marginTop: '250px', marginLeft: '500px' }}>
                 <div>
-                    <header style={{padding: '20px 25px 18px'}}>
-                        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start'}}>
-                            <div>
-                                <h1 style={{color: '#333', flexGrow: '1', fontSize: '18px', fontWeight: '500'}}>Empty Trash?</h1>
+                    <header style={{ padding: '20px 25px 18px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+                            <div style={{ height: '70px' }}>
+                                <h1 style={{ color: '#333', flexGrow: '1', fontSize: '18px', fontWeight: '500' }}>Empty Trash?</h1>
                             </div>
                             <div>
-                                <button onClick={handleClose} style={{backgroundColor: 'transparent', borderStyle: 'none', color: 'inherit', fontSize: '1em', margin: '0'}}>
-                                    <svg style={{width: "24", height: "24"}} fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <button onClick={handleClose} style={{ backgroundColor: 'transparent', borderStyle: 'none', color: 'inherit', fontSize: '1em', margin: '0' }}>
+                                    <svg style={{ width: "24", height: "24" }} fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M17.53 6.47a.75.75 0 00-1.06 0L12 10.94 7.53 6.47a.75.75 0 00-1.06 1.06L10.94 12l-4.47 4.47a.75.75 0 101.06 1.06L12 13.06l4.47 4.47a.75.75 0 101.06-1.06L13.06 12l4.47-4.47a.75.75 0 000-1.06z" fill="currentColor"></path>
                                     </svg>
                                 </button>
                             </div>
                         </div>
-                        <h2 style={{fontSize: '14px', lineHeight: '20px', color: '#737373', paddingTop: '15px'}}>
+                        <h2 style={{ fontSize: '14px', lineHeight: '20px', color: '#737373', paddingTop: '15px' }}>
                             Are you sure you want to empty the trash? All items in the trash will be permanently deleted and cannot be restored.
                         </h2>
                     </header>
                 </div>
-                <div style={{borderTop: '1px solid #d9d9d9', padding: '20px 25px', position: 'relative'}}>
-                    <div style={{display: 'flex', justifyContent: 'flex-start', flexDirection: 'row-reverse', marginTop: '0', width: '100%'}}>
-                        <button onClick={emptyAll} style={{fontSize: '14px', lineHeight: '15px', backgroundColor: '#e54e40', border: '1px solid #e54e40', color: '#fff', borderRadius: '4px', padding: '10px 15px'}}>
+                <div style={{ borderTop: '1px solid #d9d9d9', padding: '20px 25px', position: 'relative' }}>
+                    <div style={{ display: 'flex', justifyContent: 'flex-start', flexDirection: 'row-reverse', marginTop: '0', width: '100%' }}>
+                        <button onClick={emptyAll} style={{ fontSize: '14px', lineHeight: '15px', backgroundColor: '#e54e40', border: '1px solid #e54e40', color: '#fff', borderRadius: '4px', padding: '10px 15px' }}>
                             Empty Trash
                         </button>
-                        <button onClick={handleClose} style={{margin: '0 10px 0 0', backgroundColor: 'transparent', border: '1px solid #737373', color: '#737373', borderRadius: '4px', padding: '10px 15px', fontSize: '14px', lineHeight: '1'}}>
+                        <button onClick={handleClose} style={{ margin: '0 10px 0 0', backgroundColor: 'transparent', border: '1px solid #737373', color: '#737373', borderRadius: '4px', padding: '10px 15px', fontSize: '14px', lineHeight: '1' }}>
                             Cancel
                         </button>
                     </div>
--- EverQuote/client/src/components/Notes.js
+++ EverQuote/client/src/components/Notes.js
@@ -3,20 +3,16 @@ import { useSelector } from 'react-redux';
 import NoteList from './NoteList';
 import TrashList from './TrashList';
 import TextEditor from './TextEditor';
-import MoveNoteModal from './MoveNoteModal';
-import EmptyTrashModal from './EmptyTrashModal';
+
 
 
 function Notes(props) {
     const path = window.location.pathname;
     const { noteList: { display: noteList } } = useSelector(state => state.session);
-    const noteMoveModal = useSelector(state => state.ui.moveNotes);
-    const emptyTrashModal = useSelector(state => state.ui.showEmptyTrash);
+
 
     return (
-        <section style={{display: "flex"}}>
-            { noteMoveModal ? <MoveNoteModal /> : <></>}
-            { emptyTrashModal ? <EmptyTrashModal /> : <></>}
+        <section style={{ display: "flex" }}>
             {path === '/notes' ? <NoteList hidden={!noteList} /> : <TrashList hidden={!noteList} />}
             <TextEditor />
         </section>

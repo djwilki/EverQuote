1ee6005b0f8fc45287019d323405c8706e5b36cf
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-12-16 18:17:22 -0500

    Fixed error with visiting individual note resource

--- EverQuote/client/src/components/Navbar.js
+++ EverQuote/client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink, withRouter } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
 import NotebookSelect from './NotebookSelect';
 import { addNote } from '../store/notes';
-import { logout } from '../store/session';
+import { logout, setNoteList } from '../store/session';
 
 
 function Navbar({ history, userId, selectedNotebookId }) {
@@ -36,6 +36,10 @@ function Navbar({ history, userId, selectedNotebookId }) {
         setListNotebooks(!listNotebooks);
     }
 
+    const handleAllNotesClick = () => {
+        dispatch(setNoteList('notebook', null, true));
+    }
+
     return (
         <nav className={styles.navbar_container}>
             <div className={styles.navbar_content}>
@@ -63,7 +67,7 @@ function Navbar({ history, userId, selectedNotebookId }) {
                             <path fillRule="evenodd" clipRule="evenodd" d="M7.665 4.5h8.75c.92 0 1.667.746 1.667 1.667v8.748h-3.334a.625.625 0 00-.625.624v3.958H7.665c-.92 0-1.667-.747-1.667-1.667V6.167c0-.92.747-1.667 1.667-1.667zm7.037 4.584a.625.625 0 100-1.25H9.298a.625.625 0 100 1.25h5.404zm.625 2.918c0 .345-.28.625-.625.625H9.298a.625.625 0 010-1.25h5.404c.345 0 .625.28.625.625zm-4.363 4.158a.625.625 0 100-1.25H9.298a.625.625 0 100 1.25h1.666z" fill="currentColor"></path>
                             <path d="M15.373 16.164h2.157l-2.107 2.693-.05.06v-2.753z" fill="currentColor"></path>
                         </svg>
-                        <div className={styles.navlink_text}>
+                        <div onClick={handleAllNotesClick} className={styles.navlink_text}>
                             All Notes
                         </div>
                     </NavLink>
